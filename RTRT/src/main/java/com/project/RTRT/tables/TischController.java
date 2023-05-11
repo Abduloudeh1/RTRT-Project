1a9014f358e2bc69a03229fa6f712b0bd2c0f6c4
Abdul <abdul.oudeh@gmail.com>
2023-05-11 16:12:27 +0200

    Create tables package

---
+++ RTRT/src/main/java/com/project/RTRT/tables/TischController.java
@@ -0,0 +1,52 @@
+package com.project.RTRT.tables;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "table")
+public class TischController {
+    @Autowired
+    TischRepository tischRepository;
+    @GetMapping("findall")
+    public List<Tisch> getTable(){
+        // get all table
+        return tischRepository.findAll();
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<Tisch> addTable(@RequestBody Tisch table){
+        // add a new table
+        return new ResponseEntity<>(tischRepository.save(table), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("delete/{id}")
+    // Delete table
+    public ResponseEntity<Tisch> deleteTable(@PathVariable Integer id){
+        if (this.tischRepository.findById(id).isPresent()){
+            tischRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @PutMapping("update/{id}")
+    public ResponseEntity<Tisch> updateTable(@PathVariable Integer id, @RequestBody Tisch tisch){
+        if (this.tischRepository.findById(id).isPresent()){
+            tisch.setTableId(id);
+            return new ResponseEntity<>(this.tischRepository.save(tisch),HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
+
+}
